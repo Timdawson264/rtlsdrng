b4c709cd4e908fbf305b096f7cd3e77a836df588
Tim Dawson <tkd6@students.waikato.ac.nz>
2015-07-23 19:37:42 +1200

    moved kernal module to http://github.com/timdawson264/rtlsdr_timestamper added remote build script

--- module/kernelmachine.sh
+++
@@ -1,7 +0,0 @@
-qemu-system-x86_64 -readconfig /home/tim/VMs/kernel.config -smb /home/tim/src -smp 2 -m 1G -netdev user,id=mynet0,hostfwd="tcp::5555-:22" &
-
-nmap localhost -p5555 -sV | grep ssh;  
-while [ $? -ne 0 ]; do !!; done
-
-ssh  localhost -p 5555 -t "cd ~/src/rtlsdrng/module; make clean; make; sudo rmmod rtlsdr.ko -f ; sudo insmod rtlsdr.ko; dmesg -w"
-#ssh  localhost -p 5555 -t "sudo shutdown -h now"
